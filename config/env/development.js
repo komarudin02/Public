7914f7065f955072abd7f31fceca61aaa432d1e5
komarudin02 <komar.udin@reebonz.com>
2016-05-02 13:25:07 +0700

    update

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+  db: 'mongodb://root:root@ds011902.mlab.com:11902/mean-dev',
   debug: true,
   logging: {
     format: 'tiny'
